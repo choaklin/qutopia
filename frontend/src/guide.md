5e04a5d18847061bac95d359a20471815609ba4c
choaklin <cunhuigood@hotmail.com>
2018-06-10 15:48:37 +0800

    component的实践
    Signed-off-by: choaklin <cunhuigood@hotmail.com>

---
+++ frontend/src/guide.md
@@ -0,0 +1,9 @@
+[TOC]
+## 模板(template)
+- template内容【只能】有一个根节点
+
+## 组件(component)
+- html定义在 <template>，js定义在 <script>
+
+## 路由(router)
+- 一个路由是映射一个组件(component)
--- frontend/src/module/admin/components/home.vue
+++ frontend/src/module/admin/components/home.vue
@@ -1,7 +1,24 @@
 <template>
     <div>
-        here is home page
+        <span>{{msg}}</span>
+        <button v-on:click="clickButton">You clicked me {{clickCount}} times.</button>
     </div>
 </template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                clickCount: 0,
+                msg: 'Welcome to Home Page'
+            }
+        },
+        methods: {
+            clickButton: function () {
+                this.clickCount++;
+            }
+        }
+    }
+</script>
 
 
